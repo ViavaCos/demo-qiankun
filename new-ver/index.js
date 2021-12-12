49739f47ba09df3d710c3029a22e26bd96c8a2ea
ViavaCos <13811092694@163.com>
2021-12-12 17:25:06 +0800

    add vuex

---
+++ new-ver/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import ElementUI from "element-ui"
+
+
+import 'element-ui/lib/theme-chalk/index.css';
+
+const install = function(Vue){
+    Vue.use(ElementUI)
+}
+
+export default install
---
+++ old-ver/index.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+import ElementUI from "element-ui"
+import 'element-ui/lib/theme-chalk/index.css';
+
+const install = function(Vue){
+    Vue.use(ElementUI)
+}
+
+export default install
