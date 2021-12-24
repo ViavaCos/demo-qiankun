cefe2a752c8b1c85ea54c43a9c1291d981a48992
ViavaCos <13811092694@163.com>
2021-12-24 21:25:27 +0800

    update

--- child-b/src/main.js
+++ child-b/src/main.js
@@ -6,7 +6,7 @@ import store from './store'
 Vue.config.productionTip = false
 
 let instance;
-window.Vue.prototype.$test = { name: 'CHild_B' }
+Vue.prototype.$test = { name: 'CHild_B' }
 
 
 const render = function(container){
